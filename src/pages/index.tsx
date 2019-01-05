705dde3ba96937516c1788622fac74817fdef170
Ryan Hoffman <rhoffman@extended64.com>
2019-01-05 08:23:04 -0500

    Add "Fork me on GitHub"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,16 @@ import * as React from 'react';
 import { RouterProps } from '@reach/router';
 
 import Layout from '../components/layout';
-import Calculator2 from '../components/calculator'
+import Calculator from '../components/calculator'
+import GitHubForkRibbon from 'react-github-fork-ribbon'
+
+//<a href="https://github.com/tekmaven/final-jeopardy-calculator-ui" bannerColor="#101E94" />
 const IndexPage: React.SFC<RouterProps> = ({ location }) => (
   <Layout location={location}>
-    <Calculator2 id="asd" />
+    <Calculator id="asd" />
+    <GitHubForkRibbon href="https://github.com/tekmaven/final-jeopardy-calculator-ui" color="red">
+      Fork me on GitHub
+    </GitHubForkRibbon>
   </Layout>
 );
 
